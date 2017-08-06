d33b535f33486d2028e0b850cfe4a94fc228719f
James Kamana <jaykamana@gmail.com>
2017-08-06 23:58:08 +0300

    add grid buttons

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 $(function(){
 
 	var $container = $('.container');
+	var gridSize = 64;
 
-	for (var i = 0; i < 64; i++) {
-		$container.append('<div class="row row-'+i+'"></div>');
-		for(var j=0; j< 64; j++){
-			$('.row-'+i).append('<div class="cell"></div>');
+
+	var formGrid = function(){
+		$container.html('')
+		for (var i = 0; i < gridSize; i++) {
+			$container.append('<div class="row row-'+i+'"></div>');
+			for(var j=0; j< gridSize; j++){
+				$('.row-'+i).append('<div class="cell"></div>');
+			}
 		}
+
+		$('.cell').css({"width": "calc(960px/"+gridSize+")",
+						"height": "calc(960px/"+gridSize+")"
+		})
+
+		$('.cell').on('mouseenter',function(){
+			var randomColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	   		$(this).css({"background-color": randomColor})
+   		 });
 	}
 
-	$('.cell').hover(function(){
-		var randomColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-   		$(this).css({"background-color": randomColor})
-    });
+	 formGrid();
+
+    
+    $('.grid-new').on('click', function(){
+    	gridSize = parseInt(prompt("Enter grid size"));
+    	formGrid();
+    })
+
+     $('.grid-clear').on('click', function(){
+    	formGrid();
+    })
+
+
+   
 })
